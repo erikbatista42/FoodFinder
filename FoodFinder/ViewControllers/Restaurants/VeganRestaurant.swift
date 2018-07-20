7d1c8ab5ad4b001bb041e005b4dd2b19585ac1ad
ErikBatista <erikbatista42@gmail.com>
2018-07-20 12:00:25 -0700

    trying to get this to work

--- FoodFinder/ViewControllers/Restaurants/VeganRestaurant.swift
+++ FoodFinder/ViewControllers/Restaurants/VeganRestaurant.swift
@@ -6,7 +6,7 @@ import SafariServices
 import MapKit
 
 class VeganRestaurant: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
-    //changes
+    //changessss
     @IBOutlet weak var mapView: MKMapView!
     
     let activityViewController = UIAlertController()
